282640b539066f40688683f5201a9b0810b37f6c
rimvanvliet <ruud@vliet.io>
2023-02-15 20:23:19 +0100

    initial commit

---
+++ arduino_secrets_template.h
@@ -0,0 +1,3 @@
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "";    // your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
